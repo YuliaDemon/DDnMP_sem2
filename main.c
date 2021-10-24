4594bfcb6ac7b77d02ebf2d29d9a6b2eaf320dd2
YuliaDemeneva <yulichka_demeneva@mail.ru>
2021-10-24 23:12:40 +0700

    Добавила массив в ass, почистила код. Сделала функцию рабочей

--- main.c
+++ main.c
@@ -2,47 +2,29 @@
 #include <stdio.h>		/* Заголовочный файл объявляет несколько целочисленных типов и макросов */
 #include <stdint.h>		/* Заголовочный файл объявляет несколько целочисленных типов и макросов */
 
-
-/*
-Вопросы:
-1. Почему на языке Си у вас 2 функции, а на ассемблере одна ?  
-Надо сделать точно также как на языке Си. 
-Функция, которая ищет номер нуля или его адрес и функция, которая запишет всё в новый массив. 
-2. Выровнять табуляцию и пробелы в коде! Должно быть аккуратно.
-
-*/
-
-
-
-
 // Инициализируем первичный массив с числами
 int32_t array_original [] = {-1,5,1,2,0,1,7,6,7,8};
 
 // Инициализируем, т.е. объявляем указатель на новый массив. Начальное значение NULL
 int32_t* array = NULL;
-	
+
 // Объявляем прототип функции, которая будет выполнять задание
 // Указываем на массив и размерность
 int32_t* array_treatment (int32_t* array_ptr, size_t array_size);	//функция находит адр.0 и записывает значение до него в новый массив
-	
+int32_t array_upor (int32_t* array_ptr, int32_t* auto_ptr, int32_t* zero_index);
 
-int32_t array_upor (int32_t* array_ptr, size_t array_size, int32_t* zero_index);		
-	
 //Экспортируем функцию	из main.s написанную на ассемблере.
 extern void array_treatment_asm (int32_t* array_ptr, size_t array_size, int32_t* new_array_ptr );	
-	
-int32_t new_array [sizeof(array_original)/sizeof(int32_t)]= {0};// делим на 10 ??????
+extern int32_t array_zapis (int32_t* array_ptr, size_t array_size);
+int32_t new_array [sizeof(array_original)/sizeof(int32_t)]= {0};
 
 int main (void)
 {
     /*Вызываем функцию array_treatment. 
 	Функция возвратит указатель на массив соответствующий заданию. 
 	Размерность массива такая же как у оригинального массива.*/
-    
 	array = array_treatment(array_original, sizeof(array_original)/sizeof(int32_t));
-	
-	array_treatment_asm(array_original, sizeof(array_original), new_array); // переход в асемблер 
-
+	array_treatment_asm(array_original, sizeof(array_original), new_array); // переход в асемблер
 	while (1)
 	{
 	}
@@ -55,32 +37,29 @@ int32_t* array_treatment (int32_t* array_ptr, size_t array_size)
 	int32_t* znach0 = 0;
 	//Выделяем область памяти для нового массива из кучи (heap).
 	new_array =  (int32_t*) (malloc(array_size));
-	
  while (number_serial<array_size)
    {
      if(array_ptr[number_serial]==0)
      {
      znach0=array_ptr+number_serial;
-			 number_serial=array_size;								
+     number_serial=array_size;
      }
-	 number_serial++;															
+	 number_serial++;
    }
-	 
-    array_upor(new_array, array_size,znach0);
-   
+    array_upor(new_array, array_ptr,znach0);
 	return new_array;
 }
 
-int32_t array_upor (int32_t* new_array, size_t array_size,int32_t* zero_index)
+int32_t array_upor (int32_t* new_array, int32_t* source_mass ,int32_t* zero_index)
 {
 	int32_t* j = (zero_index); // Вспомогательная переменная
 	int32_t i = 0;              // порядковое значение
-	int32_t* a=0;                // вспомогательная переменная		// ПОЧЕМУ УКАЗАТЕЛЬ НА ПЕРЕМЕННУЮ а ?????
+	int32_t* a=0;                // вспомогательная переменная
 	while (a < (zero_index))  
 	{
-        *(new_array+i)=*(array_original+i);  // записываем значения с индексом a в новый массив
+        *(new_array+i)=*(source_mass+i);  // записываем значения с индексом a в новый массив
         i++;
-        a=array_original+i;
-	}
+        a=source_mass+i;
+}
 }
 /********************************************************************************/
--- main_1.s
+++ main_1.s
@@ -1,4 +1,17 @@
    AREA |.text|, CODE, READONLY
+array_zapis	PROC	; Начало функции array_sum_asm
+	EXPORT array_zapis
+     MOV R1,#0
+ZAPIS
+    CMP R1,R3
+    BEQ exit
+    LDR R5,[R0,R1]; ЗАПИСЫВАЕМ В R5 
+    STR R5,[R2,R1]
+    ADD R1,#4
+    B ZAPIS
+exit
+	BX LR
+	ENDP	; Конец функции array_sum_asm
        
 array_treatment_asm	PROC	; Начало функции array_treatment_asm
 	EXPORT array_treatment_asm
@@ -16,13 +29,15 @@ VVOD
       LDR R5,[R0,R3]; ЗАПИСЫВАЕМ В R5      
       CMP R5,#0
       BEQ EXIT_FUCTION
-	  STR R5,[R2,R3]; ЗАПИСЬ ИЗ R5
       ADD R3,#4
 	  B VVOD
 	; Вызываем процедуру вычисления суммы массива
     
 EXIT_FUCTION
+    PUSH {R0-R3, LR}	; записываем все необходимые регистры в стек
+    BL array_zapis
+    POP {R0-R3, LR}
  	BX LR	; Возвращаемся назад по адресу расположенному в регистре LR
 	ENDP	; Конец функции array_treatment_asm
-
 	END		; Конец файла
+        
